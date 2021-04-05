69efb2d27c41b897b528fa2c96188b0f4e0394ff
Arturo <arturo.camacho.1dv@gmail.com>
2021-04-04 23:48:09 -0500

    Test of delete a tag in the TagController

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -12,4 +12,10 @@ class TagController extends Controller
 
         return redirect('/');
     }
+
+    public function destroy($id){
+        Tag::destroy($id);
+
+        return redirect('/');
+    }
 }
